e6afbee7eee574e7ba510a16dea51a9ff8f94d66
unknown <gupta353@purdue.edu>
2020-05-11 19:22:56 +0100

    made the code more readable

--- runoff_coefficients.m
+++ runoff_coefficients.m
@@ -15,7 +15,7 @@ clc
 direc='D:/Research/Thesis_work/Structural_vs_measurement_uncertainty/matlab_codes';
 darea=2764.0831;               % drainage area in km2
 prcp_thresh=1;               % depth of rainfall which is assumed to be negligible for the separation of hydrographs (in mm)
-length_thresh=5;               % minimum separation between the end and begining of two storms (In other words, minimum length of gap period)
+length_thresh=6;               % minimum separation between the end and begining of two storms (In other words, minimum length of gap period)
 %% load streamflow and rainfall data, and MRC
 strm_fname='streamflow_18.txt';
 filename=fullfile(direc,'huc_04100003','streamflow_data',strm_fname);
@@ -33,21 +33,28 @@ fclose(fid);
 prcp_date=data{1};
 prcp_time=data{2};
 prcp_vals=data{3};
+% replace NaNs by means of previous abd next day
+indnan=find(isnan(prcp_vals));
+for nan_ind=1:length(indnan)
+    indtmp=[indnan(nan_ind)-1;indnan(nan_ind)+1];
+    prcp_vals(indnan(nan_ind))=mean(prcp_vals(indtmp));
+end
 % convert rainfall into daily timescale from hourly timescale
 prcp_tmp=reshape(prcp_vals,24,length(prcp_vals)/24);
 prcp_vals=sum(prcp_tmp)';
+
 
 mrc_fname='MRC.txt';
 filename=fullfile(direc,'huc_04100003','MRC',mrc_fname);
 fid=fopen(filename,'r');
 mrc=textscan(fid,'%f%f','delimiter','\t','headerlines',1);
 fclose(fid);
-mrc=mrc{:,2};
+mrc=mrc{:,2};           % in cms
 
 %% Identify periods with negligible rainfall (in other words, gap-periods)
 ind_prcp_thresh=find(prcp_vals<prcp_thresh);
 differ=ind_prcp_thresh(2:end)-ind_prcp_thresh(1:end-1);
-ind_differ=find(differ>1);
+ind_differ=find(differ>1);                                                      % indices of gap time-steps
 eind_gap_periods=[ind_prcp_thresh(ind_differ);ind_prcp_thresh(end)];            % end index of each gap-period
 bind_gap_periods=[ind_prcp_thresh(1);ind_prcp_thresh(ind_differ(1:end)+1)];     % begin index of each gap-period
 gap_period=[bind_gap_periods,eind_gap_periods];
@@ -68,25 +75,27 @@ length_gap_periods(ind_gp,:)=[];
 % length_rain_periods=(eind_rain_periods-bind_rain_periods)+1;                       % length of gap-periods
 
 pseudo_gap_period=[0,0;gap_period];
-rain_period(:,1)=pseudo_gap_period(:,2)+1;
-rain_period(:,2)=[pseudo_gap_period(2:end,1)-1;length(strm_vals)];
+rain_period(:,1)=pseudo_gap_period(:,2)+1;                                      % begin index of rain period
+rain_period(:,2)=[pseudo_gap_period(2:end,1)-1;length(strm_vals)];              % end index of rain period
 if rain_period(end,1)>rain_period(end,2)
     rain_period(end,:)=[];
 end
 %% Identify streamflow period corresponding to each rainfall period
 %
-% strm_tot=[];
+strm_tot=[];
 for strm_ind=1:size(rain_period,1)-1
     
-    period{strm_ind}.rain=prcp_vals(rain_period(strm_ind,1):rain_period(strm_ind+1,1)-1);
-    period{strm_ind}.streamflow=strm_vals(rain_period(strm_ind,1):rain_period(strm_ind+1,1)-1);
+    bind=rain_period(strm_ind,1);                     % begin index of curerent rainfall period
+    eind=rain_period(strm_ind+1,1)-1;                 % index just before the start of next rainfall period
+    period{strm_ind}.rain=prcp_vals(bind:eind);
+    period{strm_ind}.streamflow=strm_vals(bind:eind);
 
 %     strm_tot=[strm_tot;period{strm_ind}.streamflow];
-%     plot(period{strm_ind}.streamflow);
-%     pause(1);
+%     plot(strm_tot);
+%     pause;
 end
 strm_ind=strm_ind+1;
-period{strm_ind}.rain=prcp_vals(rain_period(strm_ind,1):rain_period(strm_ind,2));
+period{strm_ind}.rain=prcp_vals(rain_period(strm_ind,1):end);
 period{strm_ind}.streamflow=strm_vals(rain_period(strm_ind,1):end);
 %}
 %% Complete the recession curve by appending the data from MRC
@@ -95,4 +104,12 @@ period=complete_hydrograph(period,mrc);
 %}
 
 %% Computation of runoff coefficient
+[runoff_coeff,strm_tmp_vol,prcp_tmp_vol]=runoff_coff_comp(period,darea);
+figure; hist(runoff_coeff);
+figure; plot(prcp_tmp_vol,runoff_coeff,'o')
 
+% figure;
+% for per_ind=1:length(period)
+% plot(period{per_ind}.streamflow);
+% pause(1);
+% end
