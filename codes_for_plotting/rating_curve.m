34b999b9da91530c80b69bfd158eafc376b74382
unknown <gupta353@purdue.edu>
2020-05-21 21:44:51 +0100

    code block for ploting USGS provided rating curves added

--- codes_for_plotting/rating_curve.m
+++ codes_for_plotting/rating_curve.m
@@ -6,12 +6,13 @@ close all
 clc
 
 direc='D:/Research/Thesis_work/Structural_vs_measurement_uncertainty/matlab_codes';
-begin_date='2008-01-01';    % begin date in yyyy-mm-dd format
-end_date='2013-12-31';      % end date in yyyy-mm-dd format
+begin_date='2009-03-13';    % begin date in yyyy-mm-dd format
+end_date='2011-03-05';      % end date in yyyy-mm-dd format
 
-%% load data
+%% rating curvees available on USGS website
+%
 fname='rc_official_filtered_04180500.txt';
-filename=fullfile(direc,'huc_04100003','streamflow_data',fname);
+filename=fullfile(direc,'huc_04100003','streamflow_data','rating_curve',fname);
 data=readtable(filename,'delimiter','\t');
 gage_height=0.3048*data.(3);                  % in m
 discharge=0.028316847*data.(2);                    % in cfs
@@ -34,4 +35,30 @@ ind_end=find(datetime<=end_datenum,1,'last');
 scatter(gage_height(ind_begin:ind_end),discharge(ind_begin:ind_end),'filled');
 set(gca,'xscale','log','yscale','log')
 grid on
+%}
+
+%% rating curve obtaoned through communication with USGS
+%
+fname_list={'7.1.txt','8.0.txt','8.1.txt','9.0.txt','10.0.txt'};
+% figure;
 hold on
+for fname_ind=3%1:length(fname_list)
+    
+    fname=fname_list{fname_ind};
+    filename=fullfile(direc,'huc_04100003','streamflow_data','rating_curve',fname);
+    fid=fopen(filename,'r');
+    data=textscan(fid,'%f%f');
+    gage_height=data{2}*0.3048;         % in m
+    discharge=data{1}*0.028316847;      % in cms
+    
+    scatter(gage_height,discharge,'filled');
+    
+end
+legend(fname_list,'fontname','arial','fontsize',12,'location','northwest');
+legend('boxoff')
+set(gca,'fontname','arial','fontsize',12)
+% set(gca,'xscale','log','yscale','log')
+grid on
+xlabel('Gage height (m)','fontname','arial','fontsize',12);
+ylabel('Streamflow (m^3 s^{-1})','fontname','arial','fontsize',12);
+%}
