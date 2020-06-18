176176330f4acfdf78d1ec11563763d1151e7790
unknown <gupta353@purdue.edu>
2020-06-18 16:29:55 +0100

    chnaged the specification of input arguments

--- rc_analysis/rc_est.m
+++ rc_analysis/rc_est.m
@@ -1,7 +1,7 @@
 % This routine specifies a multi-segment rating curve where each segment is
 % modeled as power-law
 % inputs: h=gage heights at which log of streamflow values are to be estimated (in m)
-%         a_list = list of multiplier paramters for different segments of
+%         a1 =  multiplier paramters for different segments of
 %         the rating curve
 %         b_list = list of exponent paramters for different segment of
 %         the rating curve
@@ -13,11 +13,13 @@
 %         'arithmetic'
 % output: log_Q = logarithm of Q (Q in m^3 s^{-1})
 
-function log_Q = rc_est(h,h_s,a_list,b_list,h0_list,aspace)
+function log_Q = rc_est(h,h_s,a1,b_list,h0_list,aspace)
     
     if strcmp(aspace,'arithmetic')
         a_list=log(a_list);
     end
+    a_list = rc_continuity(a1,h_s,h0_list,b_list,aspace);
+    
     % compute log of streamflow
     u_brk=h_s(2:end);   % upper break point of each rating curve segment
     for h_ind=1:length(h)
