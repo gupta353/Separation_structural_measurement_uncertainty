115cf54d4a9cf6facf662f7906f76e528b6759c2
unknown <gupta353@purdue.edu>
2020-06-18 21:39:13 +0100

    modofied the codeblock using the parameter 'aspace'

--- rc_analysis/rc_est.m
+++ rc_analysis/rc_est.m
@@ -16,9 +16,9 @@
 function log_Q = rc_est(h,h_s,a1,b_list,h0_list,aspace)
     
     if strcmp(aspace,'arithmetic')
-        a_list=log(a_list);
+        a1=log(a1);
     end
-    a_list = rc_continuity(a1,h_s,h0_list,b_list,aspace);
+    a_list = rc_continuity(a1,h_s,h0_list,b_list);
     
     % compute log of streamflow
     u_brk=h_s(2:end);   % upper break point of each rating curve segment
