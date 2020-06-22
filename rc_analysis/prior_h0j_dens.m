2b84d74265cceb274522e5405ec147f6d96f208b
unknown <gupta353@purdue.edu>
2020-06-22 18:39:57 +0100

    appropriate condition for length of teh array h_s added

--- rc_analysis/prior_h0j_dens.m
+++ rc_analysis/prior_h0j_dens.m
@@ -6,10 +6,9 @@
 
 function dens=prior_h0j_dens(h_0j,h0_min,h0_max,h_s)
     
-    % uniform prior
-    h_s(1)=[]; h_s(end)=[]; % delete h01 and 1000 from h_s
-    
-    if ~isempty(h_s)
+    % uniform prior  
+    if length(h_s)>2
+        h_s(1)=[]; h_s(end)=[]; % delete h01 and 1000 from h_s
         llimit=h0_min*ones(1,length(h_s));
         ulimit=h_s;
         n=length(h_s);
