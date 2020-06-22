125845f77656caa605eae023067efee7c0b9b3b7
unknown <gupta353@purdue.edu>
2020-06-22 21:10:36 +0100

    added beta distribtuion for sampling exponent, multiplier and first cease-to-flow- parameter

--- rc_analysis/propMCMCrnd.m
+++ rc_analysis/propMCMCrnd.m
@@ -41,9 +41,15 @@ function theta_new=propMCMCrnd(theta_old,lambda,hmin,hmax,amin,amax,bmin,bmax,al
     
     %% draw h01
     if m_new==m_old
-        half_range=min(abs(h01_old-h0_max),abs(h01_old-h0_min));
-        sigma_h01=half_range/3;
-        h01_new=h01_old+normrnd(0,sigma_h01);
+        % normal distribution
+%         half_range=min(abs(h01_old-h0_max),abs(h01_old-h0_min));
+%         sigma_h01=half_range/3;
+%         h01_new=h01_old+normrnd(0,sigma_h01);
+
+        % beta distribution
+        h01_min_tmp=max(h01_old-0.3,h0_min);
+        h01_max_tmp=min(h01_old+0.3,h0_max);
+        h01_new=h01_min_tmp+(h01_max_tmp-h01_min_tmp)*betarnd(2,2);
     else
         h01_new=prior_h01(h0_min,h0_max,nsamp);
     end
@@ -72,11 +78,17 @@ function theta_new=propMCMCrnd(theta_old,lambda,hmin,hmax,amin,amax,bmin,bmax,al
     if m_new==m_old
         b_list_new=b_list_old;
         for b_ind=1:length(b_list_new)
-            btmp=b_list_old(b_ind);
-            half_range=min(abs(btmp-bmin),abs(btmp-bmax));
-            sigma_b=half_range/3;
+            % normal distribution
+            %             btmp=b_list_old(b_ind);
+            %             half_range=min(abs(btmp-bmin),abs(btmp-bmax));
+            %             sigma_b=half_range/3;
+            %             b_list_new(b_ind)=btmp+normrnd(0,sigma_b);
             
-            b_list_new(b_ind)=btmp+normrnd(0,sigma_b);
+            % beta distribution
+            btmp=b_list_old(b_ind);
+            bmin_tmp=max(btmp-0.3,bmin);
+            bmax_tmp=min(btmp+0.3,bmax);
+            b_list_new(b_ind)=bmin_tmp+(bmax_tmp-bmin_tmp)*betarnd(2,2);
         end
     else
         b_list_new=prior_b(bmin,bmax,m_new)';
@@ -84,9 +96,15 @@ function theta_new=propMCMCrnd(theta_old,lambda,hmin,hmax,amin,amax,bmin,bmax,al
     
     %% draw multiplier parameters
     if m_new==m_old
-        half_range=min(abs(a1_old-amin),abs(a1_old-amax));
-        sigma_a=half_range/3;
-        a1_new=a1_old+normrnd(0,sigma_a);
+        % normal distribution
+%         half_range=min(abs(a1_old-amin),abs(a1_old-amax));
+%         sigma_a=half_range/3;
+%         a1_new=a1_old+normrnd(0,sigma_a);
+        
+        % beta distribtuion
+        amin_tmp=max(a1_old-0.3,amin);
+        amax_tmp=min(a1_old+0.3,amax);
+        a1_new=amin_tmp+(amax_tmp-amin_tmp)*betarnd(2,2);
     else
         a1_new=prior_a(amin,amax,nsamp);
     end
