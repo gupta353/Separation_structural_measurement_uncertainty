e17106d1295cceecf5775fab92ee092d2bc28654
unknown <gupta353@purdue.edu>
2020-09-28 18:44:09 +0100

    minor chnages

--- post_cov_Jefferysprior.m
+++ post_cov_Jefferysprior.m
@@ -1,20 +1,20 @@
 % Posterior distribtuion of covariance matrix (Sigma) with Jefferys prior as
 % non-informative prior of Sigma
 
-% clear all
-% close all
-% clc
+clear all
+close all
+clc
 
 direc='D:/Research/Thesis_work/Structural_vs_measurement_uncertainty/matlab_codes';
 N=1000; % number of covariance matrices to be drawn
 
 % load observed streamflow data (ML estimated)
-fname='streamflow_200.mat';
+fname='streamflow_18.mat';
 filename=fullfile(direc,'huc_04100003','SJRW_ML_results',fname);
 load(filename);                     % streamflow in cfs
 strm_ML=MC1_temp*0.028316847;       % streamflow in cms
 strm_ML=strm_ML(1:365,:);            % retain only one year of data
-break
+
 % define parameters of prior  (inverse Wishart distribtuion)
 [d,n]=size(strm_ML); % d=dimensions, n=number of observations
 nu=0;           % degrees of freedom
@@ -38,7 +38,7 @@ end
 eigenvalues=eig(Q);
 log_deter_Q=sum(log(abs(eigenvalues)));
 deter_Q=exp(log_deter_Q);
-break
+
 % define parameters of posterior inverse wishart distribution
 nu_s=nu+n;
 Lambda_s=Lambda+Q;
@@ -50,7 +50,7 @@ for samp_ind=1:N
     Cr(:,:,samp_ind)=corrcov(C(:,:,samp_ind));        % samples of correlation matrix from posterior distribution
        
 end
-
+break
 % save the samp and R
 fname='covmat.mat';
 save_filename=fullfile(direc,'huc_04100003','results','covmat_Jefferys_prior',fname);
