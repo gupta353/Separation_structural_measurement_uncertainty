9aa168dbeac08268df7122c8fbb24c7abb5d11b1
unknown <gupta353@purdue.edu>
2020-09-28 18:38:45 +0100

    minor changes

--- codes_for_plotting/rating_curve.m
+++ codes_for_plotting/rating_curve.m
@@ -11,12 +11,12 @@ end_date='2011-03-05';      % end date in yyyy-mm-dd format
 
 %% rating curvees available on USGS website
 %
-fname='rc_official_filtered_04180500.txt';
+fname='rc_04180500.txt';
 filename=fullfile(direc,'huc_04100003','streamflow_data','rating_curve',fname);
 data=readtable(filename,'delimiter','\t');
-gage_height=0.3048*data.(3);                  % in m
+gage_height=0.3048*data.(1);                  % in m
 discharge=0.028316847*data.(2);                    % in cfs
-date=data.(1);
+date=data.(3);
 
 datetime_wrapper=@(x)datenum(x,'mm/dd/yyyy');
 datetime=cellfun(datetime_wrapper,date);
@@ -27,13 +27,14 @@ gage_height(ind)=[];
 discharge(ind)=[];
 datetime(ind)=[];
 %% plot rating curve
+%{
 begin_datenum=datenum(begin_date,'yyyy-mm-dd');
 end_datenum=datenum(end_date,'yyyy-mm-dd');
 
 ind_begin=find(datetime>=begin_datenum,1,'first');
 ind_end=find(datetime<=end_datenum,1,'last');
 scatter(gage_height(ind_begin:ind_end),discharge(ind_begin:ind_end),'filled');
-set(gca,'xscale','log','yscale','log')
+% set(gca,'xscale','log','yscale','log')
 grid on
 %}
 
@@ -42,7 +43,7 @@ grid on
 fname_list={'7.1.txt','8.0.txt','8.1.txt','9.0.txt','10.0.txt'};
 % figure;
 hold on
-for fname_ind=3%1:length(fname_list)
+for fname_ind=1:length(fname_list)
     
     fname=fname_list{fname_ind};
     filename=fullfile(direc,'huc_04100003','streamflow_data','rating_curve',fname);
@@ -57,7 +58,7 @@ end
 legend(fname_list,'fontname','arial','fontsize',12,'location','northwest');
 legend('boxoff')
 set(gca,'fontname','arial','fontsize',12)
-% set(gca,'xscale','log','yscale','log')
+set(gca,'xscale','log','yscale','log')
 grid on
 xlabel('Gage height (m)','fontname','arial','fontsize',12);
 ylabel('Streamflow (m^3 s^{-1})','fontname','arial','fontsize',12);
