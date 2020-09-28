06de265587e1747946cdb90635d15b3112199311
unknown <gupta353@purdue.edu>
2020-09-28 18:56:45 +0100

    variables 'mrc' and 'length_gp_periods redefined', some comments modified, NaN check for precipitation data added

--- runoff_coefficients.m
+++ runoff_coefficients.m
@@ -7,16 +7,16 @@
 %           the previous hydrograph, to correct for over-estimation of
 %           streamflow appended streamflows in previous hydrographs is adjusted
 %           such that negative streamflows disappear
-
+% Def: Gap period: a time-period with negligible rainfall
 clear all
 close all
 clc
 
 direc='D:/Research/Thesis_work/Structural_vs_measurement_uncertainty/matlab_codes';
 darea=2764.0831;               % drainage area in km2
-prcp_thresh=1;               % depth of rainfall which is assumed to be negligible for the separation of hydrographs (in mm)
+prcp_thresh=1;               % depth of rainfall which is assumed to be negligible for the separation of hydrographs (in mm/day)
 length_thresh=7;               % minimum separation between the end and begining of two storms (In other words, minimum length of gap period)
-%% load streamflow and rainfall data, and MRC
+%% load streamflow data, rainfall data, and MRC
 strm_fname='streamflow_18.txt';
 filename=fullfile(direc,'huc_04100003','streamflow_data',strm_fname);
 fid=fopen(filename,'r');
@@ -33,12 +33,17 @@ fclose(fid);
 prcp_date=data{1};
 prcp_time=data{2};
 prcp_vals=data{3};
-% replace NaNs by means of previous abd next day
+% replace NaNs by means of previous and next day
 indnan=find(isnan(prcp_vals));
 for nan_ind=1:length(indnan)
     indtmp=[indnan(nan_ind)-1;indnan(nan_ind)+1];
     prcp_vals(indnan(nan_ind))=mean(prcp_vals(indtmp));
 end
+
+if isnan(prcp_vals)
+    error('precipitation data contains NaNs')
+end
+
 % convert rainfall into daily timescale from hourly timescale
 prcp_tmp=reshape(prcp_vals,24,length(prcp_vals)/24);
 prcp_vals=sum(prcp_tmp)';
@@ -49,7 +54,7 @@ filename=fullfile(direc,'huc_04100003','MRC',mrc_fname);
 fid=fopen(filename,'r');
 mrc=textscan(fid,'%f%f','delimiter','\t','headerlines',1);
 fclose(fid);
-mrc=mrc{:,2};           % in cms
+mrc=mrc{2};           % in cms
 
 %% Identify periods with negligible rainfall (in other words, gap-periods)
 ind_prcp_thresh=find(prcp_vals<prcp_thresh);
@@ -63,7 +68,7 @@ length_gap_periods=(eind_gap_periods-bind_gap_periods)+1;                       
 % remove gap periods that are smaller in length than length_thresh
 ind_gp=find(length_gap_periods<length_thresh);
 gap_period(ind_gp,:)=[];
-length_gap_periods(ind_gp,:)=[];
+length_gap_periods(ind_gp)=[];
 %% Identify rainfall periods using gap periods
 pseudo_gap_period=[0,0;gap_period];
 rain_period(:,1)=pseudo_gap_period(:,2)+1;                                      % begin index of rain period
@@ -71,6 +76,7 @@ rain_period(:,2)=[pseudo_gap_period(2:end,1)-1;length(strm_vals)];              
 if rain_period(end,1)>rain_period(end,2)
     rain_period(end,:)=[];
 end
+
 %% Identify streamflow period corresponding to each rainfall period
 %
 strm_tot=[];
@@ -89,6 +95,7 @@ strm_ind=strm_ind+1;
 period{strm_ind}.rain=prcp_vals(rain_period(strm_ind,1):end);
 period{strm_ind}.streamflow=strm_vals(rain_period(strm_ind,1):end);
 %}
+
 %% Complete the recession curve by appending the data from MRC
 %
 period=complete_hydrograph(period,mrc);
